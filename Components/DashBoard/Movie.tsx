3af9fba00abc4a2ff5ff195582d75663b6219482
nitesh <niteshsemwal15@gmail.com>
2024-05-15 02:13:52 +0530

    Ui-3

--- Components/DashBoard/Movie.tsx
+++ Components/DashBoard/Movie.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 "use client"
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { motion } from 'framer-motion'
 import { ArrowBigDownDash, PlayIcon, ShoppingBasketIcon } from 'lucide-react'
 import MovieDetail from './MovieDetail'
 import axios from 'axios'
 import movieTrailer from 'movie-trailer'
-import { url } from 'inspector'
 import ReactPlayer from "react-player";
 
 const opts = {
@@ -57,16 +57,19 @@ export default function Movie({MovieId}:{MovieId:number}) {
 
   const [moviedata,setMovieData]=useState<MovieInfo>();
   const [trailer,setTrailer]=useState<string>("");
-  useEffect(()=>{
+  const player=useRef<ReactPlayer | null>(null);
+   const [isPlaying,setPlaying]=useState<boolean>(false)
 
+  useEffect(()=>{
     async function getTrailer() {
       if(moviedata &&  "title" in moviedata){
         movieTrailer(null ,{ tmdbId: moviedata.id })
         .then((url)=>{
           console.log("url is "+url);
           setTimeout(()=>{
-            setTrailer(url||"")
+            setPlaying(true);
           },7000);
+          setTrailer(url||"")
         })
         .catch((error)=> console.log(error));
 }
@@ -85,16 +88,21 @@ export default function Movie({MovieId}:{MovieId:number}) {
   },[MovieId])
   const backgroundStyle = {
     backgroundSize: 'cover',
-    backgroundImage: `url(https://image.tmdb.org/t/p/original/${moviedata?.backdrop_path})`,
+    //backgroundImage: `url(https://image.tmdb.org/t/p/original/${moviedata?.backdrop_path})`,
   }
-  return  !trailer ?(moviedata?
+  return  moviedata?
     <motion.div initial={{y:-1000}} animate={{y:0}} transition={{delay:0.5,duration:1}}
       style={backgroundStyle}
-     className='w-[100%]  bgdrop h-[70%] text-[#fff]   movieshadow bg-gray-400'>
+     className='w-[100%] relative  bgdrop h-[70%] text-[#fff]   movieshadow bg-black'>
+      {trailer &&<div onClick={(e)=>e.stopPropagation()}><ReactPlayer controls={false}  playing={isPlaying}  ref={player}
+       light={!(isPlaying) ?
+        <img src={`https://image.tmdb.org/t/p/original/${moviedata?.backdrop_path}`} 
+        className='w-[100vw] h-[100%]' alt='Thumbnail' /> 
+       :false}
+      
+      className="absolute z-[10] h-[100%] min-w-[100vw]" url={trailer}></ReactPlayer></div>
+        
+        }
       <MovieDetail obj={moviedata}/>
-     </motion.div>:<p>loading</p>):
-     <>
-     <ReactPlayer url={trailer}  playing={true} />
-     </>
-     
+     </motion.div>:<p>loading</p>
     }
--- Components/DashBoard/MovieDetail.tsx
+++ Components/DashBoard/MovieDetail.tsx
@@ -6,20 +6,18 @@ import { MovieInfo } from "./Movie";
 export default function MovieDetail({obj}:{obj:MovieInfo|undefined}){
     return (
       <>
-      {obj &&<div onClick={(e)=>e.stopPropagation()} className='h-[100%] modalcontainer'>
-        <h1 className='text-[2rem] md:text-[4rem]'>{obj.title}</h1>
+      {obj &&<div onClick={(e)=>e.stopPropagation()} className='absolute z-[11] h-[100%] modalcontainer'>
+        <h1 className='text-[2rem] md:text-[3rem]'>{obj.title}</h1>
         <p className=' pt-[0.6rem] sm:pt-[1.6rem] text: 2rem;'>
           <span className=' text-yellow-500'>Rating: {obj.vote_average} </span>
           Release date: {obj.release_date} Runtime:{ obj.runtime}
         </p>
-  
         <p style={{color: "rgb(122, 122, 122)"}}  className='  pt-[2rem] text-[1rem] sm:text-[2rem] hyphens-auto'>{obj.tagline}</p>
        
         <div className=" bg-red-500 mt-[0.2rem] sm:mt-[1rem] flex justify-center text-center p-[0.5rem] hover:bg-red-800 hover:cursor-pointer rounded-[40%] w-[3rem] sm:w-[5rem] sm:p-[1rem]">
           <div>
           <PlayIcon  />
           </div>
-         
         </div>
         </div>}</>
     )
